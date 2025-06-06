691d2ffb60bea86afdcb7fb065b4e9a2b4c75a5b
Natalya Botsman <botsmannatashaa@gmail.com>
2025-06-06 23:27:20 +0600

    Updated styles for categories table

--- frontend/src/app/admin/categories/page.tsx
+++ frontend/src/app/admin/categories/page.tsx
@@ -5,7 +5,7 @@ export default function CategoriesPage() {
         <div className="px-4 flex flex-col items-center">
             <h1 className="mt-10 mb-10 text-3xl font-semibold">Категории</h1>
 
-            <div className="overflow-x-auto bg-white shadow-md rounded-lg">
+            <div>
                 <CategoriesList/>
             </div>
         </div>
